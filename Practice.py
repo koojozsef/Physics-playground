9a359b023b0d92c572272164d91fed3eeddd3066
Koo <uid33714@contiwan.com>
2020-06-19 17:39:50 +0200

    complex exponential

--- Practice.py
+++ Practice.py
@@ -1,1 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from math import e
 
+timeline = np.arange(0,1000,1.0)
+values = timeline*0
+
+for i in timeline:
+    
+    values[int(i)]= (e ** (i*.01*1j)).imag
+
+plt.plot(timeline,values)
+plt.ylabel('some numbers')
+plt.show()
